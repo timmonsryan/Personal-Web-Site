f667442c11da65745443af743d0348417d125dce
Ryan Timmons <timmonsryan@ymail.com>
2014-10-01 00:59:35 -0500

    Changes layout.

---
+++ css/fade.js
@@ -0,0 +1,63 @@
+$(function(){
+	$("#projects-content").hide();
+	$("footer").hide();
+	$("nav").hide();
+	$("header").hide().slideDown("slow",function(){
+		$("nav").hide().slideDown("slow",function(){
+			$("#projects-content").fadeIn(800);
+			$("footer").fadeIn(800);
+		})
+	})
+});
+
+$(function(){
+	$('#about-content').hide();
+	$('#contact-content').hide();
+	$('#about-tab').click(function(){
+		$('.tab').removeClass("active");
+		$('#about-tab').addClass("active");
+		$('#projects-content').hide();
+		$('#contact-content').hide();
+		$('footer').hide();
+		$('footer').fadeIn(800);
+		$('#about-content').fadeIn(800);
+	})
+});
+
+$(function(){
+	$('#projects-tab').click(function(){
+		$('.tab').removeClass("active");
+		$('#projects-tab').addClass("active");
+		$('#about-content').hide();
+		$('#contact-content').hide();
+		$('footer').hide();
+		$('footer').fadeIn(800);
+		$('#projects-content').fadeIn(800);
+	})
+});
+
+$(function(){
+	$('#contact-tab').click(function(){
+		$('.tab').removeClass("active");
+		$('#contact-tab').addClass("active");
+		$('#about-content').hide();
+		$('#projects-content').hide();
+		$('footer').hide();
+		$('footer').fadeIn(800);
+		$('#contact-content').fadeIn(800);
+	})
+});
+
+$(function(){
+	$('.project-info').hide();
+	$('#RecordStoreBtn').click(function(){
+		$('#RecordStoreInfo').slideToggle(500);
+	})
+});
+
+$(function(){
+	$('.project-info').hide();
+	$('#PersonalSiteBtn').click(function(){
+		$('#PersonalSiteInfo').slideToggle(500);
+	})
+});
